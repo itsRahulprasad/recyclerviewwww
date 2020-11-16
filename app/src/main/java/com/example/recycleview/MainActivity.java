8f8258f1edb5fabca64993e7ec9d8ca5ffe34cea
Rahul prasad <prasadrahul9719@gmail.com>
2020-11-16 10:56:33 +0530

    recycle view done

--- app/src/main/java/com/example/recycleview/MainActivity.java
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.recycleview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView rcv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rcv=(RecyclerView)findViewById(R.id.rclview);
+        rcv.setLayoutManager(new LinearLayoutManager(this));
+
+
+        String arr[]={"one","two","three","four","five","six","seven","one","two","three","four","five","six","seven","one","two","three","four","five","six","seven","one","two","three","four","five","six","seven","one","two","three","four","five","six","seven",};
+        rcv.setAdapter(new myAdapter(arr));
+
     }
+
 }
